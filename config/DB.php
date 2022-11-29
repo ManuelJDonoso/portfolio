a95454328d08e60dc4905c114399b108f3ea6b46
Manuel Jesús Donoso Pérez <developer@manueldonoso.es>
2022-11-29 14:00:37 +0100

    getProyectsByTechnologies

--- config/DB.php
+++ config/DB.php
@@ -16,33 +16,30 @@ class DB {
         return $conexion;
     }
    
-    public static function textConexion(){
-      $conexion = self::getConnection();
-      return $conexion;
-    }
+
 
     //obtener las tecnologias y lenguajes usados en un proyecto
-    public static function getTecnologiesProyect($proyect_tecnologies){
+    public static function getTechnologiesProyect($proyect_technologies){
         
         $conexion = self::getConnection();
         try{
-            $sql= "SELECT tecnologia FROM proyectos INNER JOIN union_proyect_tecno on union_proyect_tecno.proyecto_fk = proyectos.id_proyectos INNER JOIN languages_technologies on union_proyect_tecno.tecnologia_fk = languages_technologies.id_tecnologias WHERE proyectos.id_proyectos='".$proyect_tecnologies."'";
+            $sql= "SELECT tecnologia FROM proyectos INNER JOIN union_proyect_tecno on union_proyect_tecno.proyecto_fk = proyectos.id_proyectos INNER JOIN languages_technologies on union_proyect_tecno.tecnologia_fk = languages_technologies.id_tecnologias WHERE proyectos.id_proyectos='".$proyect_technologies."'";
             
             $resultado = $conexion->query($sql);
-            $tecnologies = array();
+            $technologies = array();
 
             if ($resultado) {
                     $row = $resultado->fetch();
                     while ($row != null) {
 
-                        $tecnologies[] = $row;
+                        $technologies[] = $row;
                         $row = $resultado->fetch();
                     }
                 }
 
                 $conexion = null;
 
-                return $tecnologies;
+                return $technologies;
         } catch (PDOException $e) {
                 echo "ERROR - No se pudieron obtener las tecnologias: " . $e->getMessage();
         }
@@ -62,8 +59,8 @@ class DB {
             if ($resultado) {
                     $row = $resultado->fetch();
                     while ($row != null) {
-                        $tecnologies =self::getTecnologiesProyect($row["id_proyectos"]);   
-                        $row[]=$tecnologies;
+                        $technologies =self::getTechnologiesProyect($row["id_proyectos"]);   
+                        $row[]=$technologies;
                         $proyects[] = $row;
                         $row = $resultado->fetch();
                     }
@@ -91,8 +88,8 @@ class DB {
             if ($resultado) {
                     $row = $resultado->fetch();
                     while ($row != null) {
-                        $tecnologies =self::getTecnologiesProyect($row["id_proyectos"]);   
-                        $row[]=$tecnologies;
+                        $technologies =self::getTechnologiesProyect($row["id_proyectos"]);   
+                        $row[]=$technologies;
                         $proyects[] = $row;
                         $row = $resultado->fetch();
                     }
@@ -107,5 +104,52 @@ class DB {
     
     }
 
+
+    //obtener  proyectos con las tecnologias o lenguajes  
+    public static function getProyectsByTechnologies( $ArrayTechnologies){
+        
+        $conexion = self::getConnection();
+        try{
+            $sql= "SELECT id_proyectos,titulo,Title,parrafos,paragraph, dir_img dir_proyect, dir_code FROM proyectos INNER JOIN union_proyect_tecno on union_proyect_tecno.proyecto_fk = proyectos.id_proyectos INNER JOIN languages_technologies on union_proyect_tecno.tecnologia_fk = languages_technologies.id_tecnologias WHERE tecnologia='php' or tecnologia='html' GROUP BY id_proyectos";
+            
+            $resultado = $conexion->query($sql);
+            $proyects = array();
+
+            if ($resultado) {
+                    $row = $resultado->fetch();
+                    while ($row != null) {
+                         $technologies =self::getTechnologiesProyect($row["id_proyectos"]);   
+                         $temTechnologies=array();
+                         //var_dump ($technologies);
+                         foreach ($technologies as $key => $value) {
+                            $temTechnologies [] =$value["tecnologia"];
+                         }
+
+                       $valid =true;
+                         
+                        foreach ($ArrayTechnologies as $key => $value) {
+                           $existe = in_array($value,$temTechnologies);
+                            if(!$existe) $valid=false;
+                        }
+                        //  var_dump($existe);
+                        //  var_dump($temTechnologies);
+                        //  var_dump($ArrayTechnologies);
+                         var_dump($valid);
+                         
+                         $row[]=$technologies;
+                        $proyects[] = $row;
+                        $row = $resultado->fetch();
+                    }
+                }
+
+                $conexion = null;
+
+                return $proyects;
+        } catch (PDOException $e) {
+                echo "ERROR - No se pudieron obtener los proyectos: " . $e->getMessage();
+        }
+    
+    }
+    
 
 }
